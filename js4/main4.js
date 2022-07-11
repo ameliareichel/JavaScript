e720dbcc8b3e312fd1de07b705081b001ce6f7e6
amelia reichel <ameliareichel7@gmail.com>
2022-07-11 17:41:30 +0200

    js suma zmmiennych zadanie 34

---
+++ js4/main4.js
@@ -0,0 +1,10 @@
+let firstNumber = 7;
+let secondNumber = 8;
+
+function sum(n1,n2) {
+    let result = n1 + n2;
+    return result;
+}
+
+let firstResult = sum(firstNumber,secondNumber);
+console.log(firstResult);
