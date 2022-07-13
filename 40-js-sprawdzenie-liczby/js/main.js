6b25351dc147e8c6d835ea7a3e1d45f9fe597b94
amelia reichel <ameliareichel7@gmail.com>
2022-07-13 12:46:09 +0200

    js sprawdzenie liczby zadanie 40

---
+++ 40-js-sprawdzenie-liczby/js/main.js
@@ -0,0 +1,11 @@
+function numCheck(n) {
+    if (n>100 && n<200) {
+        console.log("Liczba znajduje się w przedziale");
+    }
+    else {
+        console.log("Liczba nie znajduje się w przedziale");
+    }
+}
+console.log(numCheck(150));
+console.log(numCheck(222));
+console.log(numCheck(77));
