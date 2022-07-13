85d9346294135f62562c7926625d17df248bab56
amelia reichel <ameliareichel7@gmail.com>
2022-07-13 11:01:43 +0200

    js wieksza liczba zadanie 37

---
+++ 37-js-wieksza-liczba/js/main.js
@@ -0,0 +1,14 @@
+let num1 = 2;
+let num2 = 5;
+
+function greaterNum(n1,n2) {
+    if (n1>n2) {
+        return n1;
+    }
+    else {
+        return n2;
+    }
+}
+
+console.log(greaterNum(num1,num2));
+console.log(greaterNum(90,1));
---
+++ 41-js-sprawdzanie-liczby2/js/main.js
@@ -0,0 +1,12 @@
+function trueOrFalse(n1,n2) {
+    if (n1 === 20 || n1+n2 <= 20 || n2 === 20) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(trueOrFalse(40,7));
+console.log(trueOrFalse(20,7));
+console.log(trueOrFalse(2,7));
